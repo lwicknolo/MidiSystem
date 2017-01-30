2812d4214559ac4a04b862a192513ce2649df343
dnovikov <prom1988@yandex.ru>
2017-01-30 20:16:39 +0200

    delete some code

--- src/com/headfirst/MiniMusicCmdLine.java
+++ src/com/headfirst/MiniMusicCmdLine.java
@@ -32,8 +32,6 @@ public class MiniMusicCmdLine {
             Sequence seq = new Sequence(Sequence.PPQ,4);
             Track track = seq.createTrack();
 
-            MidiEvent event = null;
-
             ShortMessage first = new ShortMessage();
             first.setMessage(192, 1, instrument, 0);
             MidiEvent changeInstrument = new MidiEvent(first, 1);
